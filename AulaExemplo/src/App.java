0a9e531f79df7f7341fa3fbdeeedbba9e0559d43
Yann Martins <102706324+YannMartins@users.noreply.github.com>
2023-06-04 19:15:17 -0300

    Escopo de variáveis

---
+++ AulaExemplo/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args){
+        var x = 10;
+
+        for (var i = 0; i < 10; i++) {
+            var a = i*2;
+            System.out.println(a);
+            System.out.println(x);
+        }
+    }
+}
