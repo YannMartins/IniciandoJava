ac860614b247337430dcec177f66131c431ce11e
YannMartins <yann-martins@hotmail.com>
2023-10-12 23:17:07 -0300

    Utilizando métodos em um novo projeto

---
+++ SistemaSmartTv/src/Usuario.java
@@ -0,0 +1,24 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.desligar();
+        System.out.println( "Novo status -> A TV está ligada? " + smartTv.ligada);
+
+        smartTv.ligar();
+        System.out.println( "Novo status -> A TV está ligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+    }
+}
